3d1dbfc4218ec50e72a660b7e01fbf4914910542
Agustin Aguilera <agustin.aguilera424@gmail.com>
2021-07-21 21:51:24 -0300

    plant

--- dist/objects/plants/plant.js
+++ dist/objects/plants/plant.js
@@ -1,3 +1,19 @@
 "use strict";
-console.log("Im a plant");
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Plant = void 0;
+class Plant {
+    //Constructor
+    constructor(year, height) {
+        this.year = year;
+        this.height = height;
+    }
+    //Methods
+    getYear() {
+        return this.year;
+    }
+    getHeight() {
+        return this.height;
+    }
+}
+exports.Plant = Plant;
 //# sourceMappingURL=plant.js.map
--- objects/plants/plant.ts
+++ objects/plants/plant.ts
@@ -1,1 +1,26 @@
-console.log("Im a plant")
+export abstract class Plant{
+    //Vars
+    private year: number;
+    private height: number;
+    
+    //Constructor
+    constructor(year:number, height:number){
+        this.year=year;
+        this.height=height;
+    }
+
+    //Methods
+    getYear():number{
+        return this.year;
+    } 
+    getHeight():number{
+        return this.height;
+    } 
+
+    //Abstract Methods
+    abstract hs():number;
+    abstract isStrong():boolean;
+    abstract isFather():number;
+    abstract size():number;
+    abstract perfectPlot():boolean;
+}
