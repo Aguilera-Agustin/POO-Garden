7948e67f4799fd2a497a6f10a2b4573f599e7235
Agustin Aguilera <agustin.aguilera424@gmail.com>
2021-07-21 22:45:13 -0300

    Peppermint

---
+++ objects/plants/variety/peppermint.ts
@@ -0,0 +1,13 @@
+import { Plot } from '../../plots/plot';
+import { Mint } from '../mint';
+
+export class Peppermint extends Mint{
+    
+    constructor(year: number, height: number){
+        super(year, height);
+    }
+
+    size():number{
+        return super.getHeight();
+    }
+}
