c443dbecce3de2d1710964659fdba4799c66252e
Agustin Aguilera <agustin.aguilera424@gmail.com>
2021-07-21 22:21:54 -0300

    mint plant

---
+++ objects/plants/mint.ts
@@ -0,0 +1,25 @@
+import { Plot } from '../plots/plot';
+import { Plant } from './plant';
+
+export class Mint extends Plant{
+    private sunHs:number;
+    constructor(year: number, height: number){
+        super(year, height);
+        this.sunHs=6;
+    }
+
+    hs(): number {
+        return this.sunHs;
+    }
+  
+    isFather(): boolean {
+        return (super.isStrong() || super.getHeight()>0.4);
+    }
+
+    size(): number {
+        return (super.getHeight() * 3);
+    }
+    perfectPlot(plot: Plot): boolean {
+        throw new Error('Method not implemented.');
+    }
+}
