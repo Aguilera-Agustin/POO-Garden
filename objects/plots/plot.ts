946650ac99eb3855f641139b3f09da6dfef804ac
Agustin Aguilera <agustin.aguilera424@gmail.com>
2021-07-21 22:03:46 -0300

    base plot

--- objects/plots/plot.ts
+++ objects/plots/plot.ts
@@ -1,1 +1,13 @@
-console.log("Im plot")
+import { Plant } from '../plants/plant';
+export class Plot {
+    private width :number;
+    private lengthty :number;
+    private sunHs :number;
+    private plants :Plant[];
+    constructor(size:{width: number, lengthty: number}, sunHs:number, plants:Plant[]){
+        this.width=size.width;
+        this.lengthty=size.lengthty;
+        this.sunHs=sunHs;
+        this.plants=plants;
+    }
+}
