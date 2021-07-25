63ae0e83bf15a6a611d262c05bf2a80a0102a949
Agustin Aguilera <agustin.aguilera424@gmail.com>
2021-07-25 10:53:34 -0300

    main

--- app.ts
+++ app.ts
@@ -1,1 +1,10 @@
-console.log("Hello world :D !")
+const fs = require('fs')
+fs.readFile('public/plant.txt', (err: any, bfr: any)=>{
+    if(!err){
+        console.log(bfr.toString())
+        console.log("Hi, my name is Agustin and i created this program for UNAHUR")
+    }
+    else{
+        console.log(err)
+    }
+})
