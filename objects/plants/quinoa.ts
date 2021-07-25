b979ccae7b638fa31778a225bf48e58288edf978
Agustin Aguilera <agustin.aguilera424@gmail.com>
2021-07-25 10:34:46 -0300

    filter --> find

--- objects/plants/quinoa.ts
+++ objects/plants/quinoa.ts
@@ -20,6 +20,6 @@ export class Quinoa extends Plant{
         return 0.5;
     }
     perfectPlot(plot: Plot): boolean {
-        return plot.plotPlants().filter((eachPlant)=>(eachPlant.getHeight()>1.5))?(false):(true)
+        return plot.plotPlants().find((eachPlant)=>(eachPlant.getHeight()>1.5))?(false):(true)
     }
 }
